ef894ffed452489968efb3242a4ea8b3eb054e7f
dulma12722 <yukiho27mst@gmail.com>
2025-04-20 22:31:51 +0900

    kadai_005_20250420_Re
    再提出

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,14 @@
 // 変数の宣言・値の代入
-let bmi = 68 / (1.7 * 1.7);
+// let bmi = 68 / (1.7 * 1.7);
+
+// 変数-体重-の宣言
+let weight = 68;
+
+// 変数-身長-の宣言
+let height = 1.7;
+
+// 変数-BMI-の宣言
+let bmi = weight / (height * height);
 
 // コンソールへの出力
 console.log(bmi);
